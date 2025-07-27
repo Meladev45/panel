83ec60574aba01d54b7d908b032e10f45eeaeac5
chiemela emmanuel <chiemelae001@gmail.com>
2025-07-27 22:22:45 +0100

    last commit

---
+++ app/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between px-4 pt-4 text-black">
+      <h2>Dashboard</h2>
+      <h2>welcome Back, Clint</h2>
+    </div>
+  );
+};
+
+export default Header;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,20 @@
-import Image from "next/image";
+
+
+import Barchart from "./Components/Barchart";
+import Header from "./Components/Header";
+import Recentorder from "./Components/Recentorder";
+import Topcard from "./Components/Topcard";
 
 export default function Home() {
   return (
-  
-       <div>hello world</div>
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
+      <Topcard />
+
+      <div className="grid p-4 md:grid-cols-3 grid-cols-1 gap-4">
+        <Barchart />
+        <Recentorder />
+      </div>
+    </div>
   );
 }
